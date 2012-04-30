1146f5bcdd0cac6f59733ffa16d8a5798b02f919
Jonathan Swartz <swartz@pobox.com>
2012-04-29 21:12:07 -0700

    init

---
+++ lib/CHI/Memoize.pm
@@ -0,0 +1,41 @@
+package CHI::Memoize;
+use 5.008;
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+CHI::Memoize - Make functions faster with memoization via CHI
+
+=head1 SYNOPSIS
+
+    use CHI::Memoize;
+
+=head1 DESCRIPTION
+
+CHI::Memoize provides
+
+Questions and feedback are welcome, and should be directed to the mailing list:
+
+    http://groups.google.com/...
+
+Bugs and feature requests will be tracked at RT:
+
+    http://rt.cpan.org/NoAuth/Bugs.html?Dist=CHI-Memoize
+    bug-chi-memoize@rt.cpan.org
+
+The latest source code can be browsed and fetched at:
+
+    http://github.com/jonswar/perl-chi-memoize
+    git clone git://github.com/jonswar/perl-chi-memoize.git
+
+=head1 SEE ALSO
+
+L<Some::Module>
+
